b225b0e2df830ff8006d8f6c73d0b7a448575e60
unknown <shubham.devops.1@gmail.com>
2022-08-15 00:14:21 +0530

    sds

--- vars/sonarRunner.groovy
+++ vars/sonarRunner.groovy
@@ -12,7 +12,7 @@ void call(String mavenHome, String mavenSettings, String targetPom){
         stage("Sonar: Validate"){
             sh "pwd"
             sh "ls -ltr"
-            sh "id"
+            sh "whoami"
         }
 
         //attempt to create the sonar project advance and assign qualitygate
