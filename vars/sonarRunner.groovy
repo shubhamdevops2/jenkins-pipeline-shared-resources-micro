a24874027b803331223467bc867e2ff7d73338ef
unknown <shubham.devops.1@gmail.com>
2022-08-15 00:03:28 +0530

    sds

--- vars/sonarRunner.groovy
+++ vars/sonarRunner.groovy
@@ -36,8 +36,8 @@ void call(String mavenHome, String mavenSettings, String targetPom){
                     def artifactId = pom.artifactId
                     def groupId = pom.groupId 
 
-                    sonarKey = groupId + ":" + artifactId + ":" + main
-                    sonarProjectName = artifactId + " " + main
+                    sonarKey = groupId + ":" + artifactId + ":" + "main"
+                    sonarProjectName = artifactId + " " + "main"
                     def sonarName = pom.name
 
                     def sonarQualityGateId = sonarQubeDetails.getProjectGate(artifactId)
