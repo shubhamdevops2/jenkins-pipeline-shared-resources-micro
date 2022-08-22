a4a73534d5c49336b46d56d6a3e9cbf835a62403
unknown <shubham.devops.1@gmail.com>
2022-08-22 17:57:16 +0530

    sdsss

--- vars/singleWarBuild.groovy
+++ vars/singleWarBuild.groovy
@@ -137,7 +137,7 @@ def call(body){
                 }
 
                 stage("Update repo"){
-                    sshagent(['jenkins-docker-github-cred']){
+                    sshagent(['jenkins-github-jenkinsdocker05']){
                         sh "git config --global user.email \"jenkinsdocker05@gmail.com\" && git config --global user.name \"jenknsdocker05\" && \
                             git commit -am '[JENKINS] Built version ${releaseVersion}' && git push"
                     }    
