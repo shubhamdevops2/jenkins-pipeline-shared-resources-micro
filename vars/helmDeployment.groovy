16c4a8eb18bc90c3f9ea51ac1f68bab275766cd1
unknown <shubham.devops.1@gmail.com>
2022-08-24 18:47:35 +0530

    sds

--- vars/helmDeployment.groovy
+++ vars/helmDeployment.groovy
@@ -14,7 +14,8 @@ def call(body){
     def tagsCaptured = ""
     def findImagePath = ""
     def newyaml = ""
-    def tagPattern = /[0-9]{12}\.[a-z]{3}\.[a-zA-Z0-9-]{9}\.[a-zA-Z-.\/]{2,}\:[0-9a-zA-Z._-]{2,}/
+    //def tagPattern = /[0-9]{12}\.[a-z]{3}\.[a-z]{3}\.[a-zA-Z0-9-]{9}\.[a-zA-Z-.\/]{2,}\:[0-9a-zA-Z._-]{2,}/
+    def tagPattern = "^(?P<repository>[\w.\-_]+((?::\d+|)(?=/[a-z0-9._-]+/[a-z0-9._-]+))|)(?:/|)(?P<image>[a-z0-9.\-_]+(?:/[a-z0-9.\-_]+|))(:(?P<tag>[\w.\-_]{1,127})|)$"
     def deployImageList = []
     def dockerImagePathList = []
     def buildVersion = 0
