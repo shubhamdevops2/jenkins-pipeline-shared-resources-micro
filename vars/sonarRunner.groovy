abee91398b2a4322cfd5e1e239706245f8705e90
shubhamdevops2 <shubhamd.devops.2@gmail.com>
2022-10-30 13:52:00 +0530

    dsds

--- vars/sonarRunner.groovy
+++ vars/sonarRunner.groovy
@@ -39,7 +39,7 @@ void call(String mavenHome, String targetFile,String releaseVersion){
                     def node = readJSON file: targetFile
                     def artifactId = node.version
 
-                    sonarKey = artifactId + ":" + "main"
+                    sonarKey = node.name+":"+artifactId + ":" + "main"
                     sonarProjectName = node.name + " " +artifactId + " " + "main"
                     def sonarName = node.name
 
